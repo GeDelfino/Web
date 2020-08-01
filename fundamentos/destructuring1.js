9c57fd49b7fb1793492c2f32daebef493af4e315
gean <geanvida3@outlook.com>
2020-07-31 23:10:30 -0300

    Finalizando Fundamentos

---
+++ fundamentos/destructuring1.js
@@ -0,0 +1,23 @@
+// Novo recurso do ES2015
+
+const pessoa = {
+    nome: 'Gean',
+    idade: 21,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 10000
+    }
+}
+
+const { nome, idade } = pessoa
+
+console.log(nome, idade)
+
+const { nome: n, idade: i} = pessoa
+console.log(n, i)
+
+const { sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada)
+
+const {endereco: {  logradouro, numero, cep }   } = pessoa
+console.log(logradouro, numero, cep)
